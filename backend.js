1d7dbf370ee55dd76f9309d1edf7b17a179238af
rileylarsonpro <riley.larson.pro@gmail.com>
2021-09-13 17:56:27 -0600

    gipo 4

--- backend.js
+++ backend.js
@@ -18,23 +18,13 @@ var LED = new Gpio(11, 'out'); //use GPIO pin 4, and specify that it is output
 const server = app.listen(8080, () => {
     console.log("Started server on port 8080")
 })
-let blinkInterval = setInterval(blinkLED, 250)
-function blinkLED() { //function to start blinking
-    if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
-      LED.writeSync(1); //set pin state to 1 (turn LED on)
-    } else {
-      LED.writeSync(0); //set pin state to 0 (turn LED off)
-    }
-  }
-function endBlink() { //function to stop blinking
-    clearInterval(blinkInterval); // Stop blink intervals
-    LED.writeSync(0); // Turn LED off
-    LED.unexport(); // Unexport GPIO to free resources
-  }
-  setTimeout(endBlink, 5000); //stop blinking after 5 seconds
 // [GET] /joke endpoint
 app.get('/joke', cors(), (req,res) => {
-  
+    if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
+        LED.writeSync(1); //set pin state to 1 (turn LED on)
+      } else {
+        LED.writeSync(0); //set pin state to 0 (turn LED off)
+    }
     res.status(200).send(response)
 })
 
