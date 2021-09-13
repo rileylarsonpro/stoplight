d1b5fbd48641a4e7fbe097f9b05312ef8779be62
rileylarsonpro <riley.larson.pro@gmail.com>
2021-09-13 15:08:46 -0600

    gipo test 1

--- backend.js
+++ backend.js
@@ -1,48 +1,46 @@
 // Require express and define app object
 const express = require('express')
-const fs = require('fs')
-    let app = express()
-    let cors = require('cors')
+let app = express()
+let cors = require('cors')
+
 // Get files from public folder     
 app.use(express.static(__dirname + '/public'))
+
 // Allow use of JSON and cors
 app.use(express.json())
 app.use(cors())
+
+// Next two lines from https://www.w3schools.com/nodejs/nodejs_raspberrypi_blinking_led.asp
+var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+var LED = new Gpio(11, 'out'); //use GPIO pin 4, and specify that it is output
+
 // Start up on port 8080 
 const server = app.listen(8080, () => {
     console.log("Started server on port 8080")
 })
 
-// Random number function
-function getRandInt(max) {
-    return Math.floor( Math.random() * Math.floor(max))
-}
+function blinkLED() { //function to start blinking
+    if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
+      LED.writeSync(1); //set pin state to 1 (turn LED on)
+    } else {
+      LED.writeSync(0); //set pin state to 0 (turn LED off)
+    }
+  }
+function endBlink(blinkInterval) { //function to stop blinking
+    clearInterval(blinkInterval); // Stop blink intervals
+    LED.writeSync(0); // Turn LED off
+    LED.unexport(); // Unexport GPIO to free resources
+  }
 // [GET] /joke endpoint
 app.get('/joke', cors(), (req,res) => {
-        let file = JSON.parse(fs.readFileSync(__dirname + '/jokes.json'))
-        let error = { "error": "There are no jokes :("}
-        console.log(file)
-        if (file.length == 0) {
-            console.log("made it")
-            res.status(404).send(JSON.stringify(error))
-        }
-        else {
-            let jokeToReturn = file[getRandInt(file.length)]
-            res.send(jokeToReturn)
-        }
+    let blinkInterval = setInterval(blinkLED, 250)
+  
+    setTimeout(endBlink(blinkInterval), 5000); //stop blinking after 5 seconds
+    res.status(200).send(response)
 })
 
+
 // [POST] /joke endpoint
-app.post('/joke', cors(), (req,res) => {
-    // Get request body
-    let reqBody = req.body
-    // Get current jokes in file
-    let file = JSON.parse(fs.readFileSync(__dirname + '/jokes.json'))
-    // Add new joke to the end
-    file.push(reqBody)
-    // Write new data to file 
-    fs.writeFileSync(__dirname + '/jokes.json', JSON.stringify(file))
-    // Send confirmation 
-    let response = { message: "Added a Joke to the Bank"}
+app.get('/', cors(), (req,res) => {
     res.status(200).send(response)
 })
