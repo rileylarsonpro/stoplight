5c642100de742f413b9fc69cb9300403fbb14093
rileylarsonpro <riley.larson.pro@gmail.com>
2021-09-13 14:24:30 -0600

    Basic file structure added

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+function redClicked() {
+    console.log("Red clicked")
+}
+function yellowClicked() {
+    console.log("Yellow clicked")
+}
+function greenClicked() {
+    console.log("Green clicked")
+}
+function allOff() {
+    console.log("all off")
+}
+function toggleAuto() {
+    console.log("toggle auto")
+}
