b345033c635bc995163f43577e39df2450ca0fa0
rileylarsonpro <riley.larson.pro@gmail.com>
2021-09-13 18:17:40 -0600

    added sending of request

--- public/js/script.js
+++ public/js/script.js
@@ -2,14 +2,17 @@ const RPI_IP = "10.4.9.190:8080"
 function redClicked() {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "http://" + RPI_IP + "/red_on");
+    xhr.send()
 }
 function yellowClicked() {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "http://" + RPI_IP + "/yello_on");
+    xhr.send()
 }
 function greenClicked() {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "http://" + RPI_IP + "/green_on");
+    xhr.send()
 }
 function allOff() {
     console.log("all off")
